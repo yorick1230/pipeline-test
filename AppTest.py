2bc0f5fdd22c6f96013f9bcf22174292962baea5
Jeroen de Wolf <wolfjde@hotmail.com>
2021-04-22 02:22:00 +0200

    Create AppTest.py
    Pipeline exercise added PyTest unit test

---
+++ AppTest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import unittest
+import app
+from app import hello
+
+class TestApp(unittest.TestCase):
+  def setUp(self):
+    app.app.testing=True
+    self.app = app.app.test_client()
+
+  def test_hello(self):
+    self.assertTrue(hello() == "Hello World!")
+
+  def test_hello_serv(self):
+    rv = self.app.get('/')
+    self.assertEqual(rv.status, '200 OK')
+    self.assertEqual(rv.data, b'Hello KPN!')
+
+if __name__ == '__main__':
+  import xmlrunner
+  runner = xmlrunner.XMLTestRunner(output='reports')
+  unittest.main(testRunner=runner)
